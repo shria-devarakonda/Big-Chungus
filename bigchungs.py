8626f3c4f5af4979f05c0b9a4d3cc4cd316a7a1d
Shria Devarakonda <54900370+shria-devarakonda@users.noreply.github.com>
2020-07-16 13:18:24 +0530

    Create bigchungs.py
    sorry for shitposting, i cant control it

---
+++ bigchungs.py
@@ -0,0 +1,13 @@
+import datetime
+import turtle
+
+a = datetime.datetime.now()
+
+turtle.bgcolor("green")
+
+
+turtle.write("big chungus", align="right", font=("Comic Sans MS", 50, "normal"))
+
+turtle.write(a, align="left", font=("Comic Sans MS", 8, "normal"))
+
+turtle.mainloop()
